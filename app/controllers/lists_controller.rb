5318a40237094631672eed8e07c30bd6cea2efff
Simone Napoli <sn.simonenapoli@gmail.com>
2024-03-07 15:50:05 +0100

    completed most of it :D

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,33 @@
 class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find_by(id: params[:id])
+    if @list.nil?
+      redirect_to lists_path, alert: 'List not found.'
+    end
+  end
+
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to @list, notice: 'List created.'
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
